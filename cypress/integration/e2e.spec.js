5aa5e828e04f041ad141518a18c67dce1fd49d8c
Anderson Almeida <comercial.polopetro@gmail.com>
2021-12-02 11:16:08 -0300

    Exercício mód 12

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -10,12 +10,50 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('http:/lojaebac.ebaconline.art.br/produtos/')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        cy.get('[class="product-block grid"]')
+            // .first()
+            // .last()
+            // .eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+
     });
 
+    it('Deve adicionar produtos ao carrinho', () => {
+        var quantidade = 3
 
-})
+        cy.get('[class="product-block grid"]')
+            .contains('Ariel Roll Sleeve Sweatshirt').click()
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Purple').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando o camando customizado', () => {
+        cy.addProdutos('Aero Daily Fitness Tee', 'M', 'Black', '2') 
+    
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando o camando customizado', () => {
+        cy.addProdutos('Abominable Hoodie', 'M', 'Green', '2') 
+    
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando o camando customizado', () => {
+        cy.addProdutos('Aether Gym Pant', '36', 'Green', '2') 
+    
+    });
+
+    
+    
+}) 
+
