f0bea4b38390d46a43c860fdbbe689859f8e0c25
Anderson Almeida <comercial.polopetro@gmail.com>
2021-12-14 14:53:05 -0300

    Exercício mód 12

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -10,26 +10,19 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
+
         cy.visit('minha-conta')
+        cy.fixture('perfil').then((dados) => {
+            cy.login(dados.usuario, dados.senha)
+
+        })
+
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste.com')
-        cy.get('.woocommerce-form > .button').click()
 
         cy.visit('produtos')
-        var quantidade = 2
-
-        cy.get('[class="product-block grid"]')
-            .contains('Ariel Roll Sleeve Sweatshirt').click()
-        cy.get('.button-variable-item-XS').click()
-        cy.get('.button-variable-item-Purple').click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+        cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 'XS', 'Purple', '2')
 
         cy.visit('produtos')
         cy.addProdutos('Aero Daily Fitness Tee', 'M', 'Brown', '2')
@@ -41,23 +34,14 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.addProdutos('Aether Gym Pant', '36', 'Green', '2')
         cy.get('.woocommerce-message > .button').click()
 
+
         cy.visit('checkout')
         cy.get('#terms').click()
         cy.get('#place_order').click({ force: true })
 
 
 
-
-
-
-
-
     });
-
-
-
-
-
 
 
 
